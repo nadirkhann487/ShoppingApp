28ac29c172d38f12f83018d7f205b3c02cbd4b75
Nadir Khan <nadirkhann487@gmail.com>
2020-05-18 18:45:53 +0100

    Shooper without html

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'categories/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to:'categories#index'
+
+  resources :categories do
+  	resources :products, only: [:index]
+  end
+  get '/cart', to: 'order_items#index'
+  resources :order_items, path: '/cart/items'
+
+  get '/cart/checkout', to: 'orders#new', as: :checkout
+  patch '/cart/checkout', to: 'orders#create'
 end
